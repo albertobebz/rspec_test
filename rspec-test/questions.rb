c8d04e4e568de3d26ae1b78be373fa8d8fed9387
Alberto Ferioli <alberto_ferioli@yahoo.it>
2015-05-20 09:00:00 +0100

    more quiz resolved

--- rspec-test/questions.rb
+++ rspec-test/questions.rb
@@ -170,13 +170,13 @@ end
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
-  
+  float.ceil.to_i
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
-  
+  float.floor.to_i
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
